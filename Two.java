aa53c90c66b7dda420f0fba4e67732e6d7ba632d
Shikhar Jaiswal <144546090+Shikhar9960@users.noreply.github.com>
2025-01-22 00:09:41 +0530

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class Two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("enter number");
+        a = sc.nextInt();
+        System.out.println(a);
+    }
+    
+}
